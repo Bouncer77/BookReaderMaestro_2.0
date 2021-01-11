6f3367a6a947a8b259c9d9e8633ff67c1824290e
Ivan Kosenkov <bouncer77rus@gmail.com>
2021-01-11 11:06:19 +0300

    Добавил контроллер для главной страницы

---
+++ brm/src/main/java/com/bouncer77/brm/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.bouncer77.brm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping(value = {"/", "index"})
+    public String index(Model model) {
+        return "/templates/main.html";
+    }
+}
